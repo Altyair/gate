be55920379847b46b58cd34af0cf4c81310f06c6
Дамир <altyair@mail.ru>
2016-06-04 14:22:15 +0300

    add test.js

---
+++ test.js
@@ -0,0 +1,28 @@
+(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+   typeof define === 'function' && define.amd ? define(factory) :
+   (factory());
+}(this, function () { 'use strict';
+
+   var __commonjs_global = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : this;
+   function __commonjs(fn, module) { return module = { exports: {} }, fn(module, module.exports, __commonjs_global), module.exports; }
+
+   var gate$1 = __commonjs(function (module, exports, global) {
+   (function (global, factory) {
+      typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+      typeof define === 'function' && define.amd ? define(factory) :
+      (global.Gate = factory());
+   }(__commonjs_global, function () { 'use strict';
+
+      function CustomEventTarget() {}
+
+      function Gate (options) {};
+
+         return Gate;
+
+      }));
+   });
+
+   var a = 5;
+   console.log(window.a);
+}));
